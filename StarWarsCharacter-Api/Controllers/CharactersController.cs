5833d9a176712f3ae5fed247c71c958a6e4e373f
“Taylor <t4ybennett@gmail.com>
2023-12-09 14:03:20 +0000

    Project backend api init

---
+++ StarWarsCharacter-Api/Controllers/CharactersController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarWarsCharacter_Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CharactersController : ControllerBase
+{
+    
+}
