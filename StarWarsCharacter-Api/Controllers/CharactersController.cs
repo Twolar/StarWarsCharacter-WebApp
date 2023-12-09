2756ee034c3b9ffed93a1ef425f9e5c992654de2
“Taylor <t4ybennett@gmail.com>
2023-12-09 14:23:46 +0000

    Add initial route scaffolding for characters

--- StarWarsCharacter-Api/Controllers/CharactersController.cs
+++ StarWarsCharacter-Api/Controllers/CharactersController.cs
@@ -6,5 +6,42 @@ namespace StarWarsCharacter_Api.Controllers;
 [Route("[controller]")]
 public class CharactersController : ControllerBase
 {
-    
+    public CharactersController() { }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCharacters()
+    {
+        var characters = new List<object> {
+            new {
+                Name = "TestName1",
+                Type = "Jedi",
+                LightSaberColor = "Blue"
+            },
+            new {
+                Name = "TestName2",
+                Type = "Sith",
+                LightSaberColor = "Red"
+            },
+            new {
+                Name = "TestName3",
+                Type = "Jedi",
+                LightSaberColor = "Green"
+            },
+        };
+
+        return Ok(characters);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCharacter(int id)
+    {
+        var character = new
+        {
+            Name = "TestName2",
+            Type = "Sith",
+            LightSaberColor = "Red"
+        };
+
+        return Ok(character);
+    }
 }
