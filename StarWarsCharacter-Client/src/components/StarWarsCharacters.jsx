12d742555ba6394cb16f4c76b084f49e7af08439
“Taylor <t4ybennett@gmail.com>
2023-12-10 16:33:32 +0000

    Api calls for character related stuff + isloading functionality + minor error handling

--- StarWarsCharacter-Client/src/components/StarWarsCharacters.jsx
+++ StarWarsCharacter-Client/src/components/StarWarsCharacters.jsx
@@ -1,61 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, Grid } from "@mui/material";
+import { Box, CardActionArea, Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import axios from "../api/axios";
 
 const StarWarsCharacters = () => {
   const navigate = useNavigate();
-  const [characters, setCharacters] = useState([
-    {
-      id: 1,
-      name: "test1",
-      blurb:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu.",
-    },
-    {
-      id: 2,
-      name: "test1",
-      blurb:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      id: 3,
-      name: "test1",
-      blurb:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
-    },
-    {
-      id: 4,
-      name: "test1",
-      blurb:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isApiError, setIsApiError] = useState(false);
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        const response = await axios.get("/characters");
+        setCharacters(response?.data);
+        setIsLoading(false);
+        setIsApiError(false);
+      } catch (err) {
+        setIsApiError(true);
+        setIsLoading(false);
+        console.error(err);
+      }
+    };
+
+    getCharacters();
+  }, []);
 
   return (
-    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-      {characters.map((character) => (
-        <Grid item xs={12} sm={6} md={3} key={character.id}>
-          <Card
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              height: "100%",
-            }}
+    <Box maxWidth={750}>
+      {isLoading ? (
+        <>Loading...</>
+      ) : !isApiError ? (
+        <>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
           >
-            <CardActionArea sx={{ height: "100%" }} onClick={() => navigate(`/characters/${character.id}`)}>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {character.name}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+            {characters.map((character) => (
+              <Grid item xs={12} sm={6} md={6} key={character.id}>
+                <Card
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    height: "100%",
+                  }}
+                >
+                  <CardActionArea
+                    sx={{ height: "100%" }}
+                    onClick={() => navigate(`/characters/${character.id}`)}
+                  >
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="div">
+                        {character.name}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      ) : (
+        <>Oops, looks like an error was encountered.</>
+      )}
+    </Box>
   );
 };
 
--- StarWarsCharacter-Client/src/scenes/characterProfile/index.jsx
+++ StarWarsCharacter-Client/src/scenes/characterProfile/index.jsx
@@ -1,8 +1,32 @@
-import { Box, Typography } from "@mui/material";
-import { useParams } from "react-router-dom";
+import { Box, Typography, Card, CardContent, Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "../../api/axios";
 
 const CharacterProfile = () => {
   let { characterId } = useParams();
+  let navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isApiError, setIsApiError] = useState(false);
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    const getCharacter = async () => {
+      try {
+        const response = await axios.get(`/characters/${characterId}`);
+        console.log(response.data);
+        setCharacter(response?.data);
+        setIsLoading(false);
+        setIsApiError(false);
+      } catch (err) {
+        setIsLoading(false);
+        setIsApiError(true);
+        console.error(err);
+      }
+    };
+
+    getCharacter();
+  }, [characterId]);
 
   return (
     <Box
@@ -12,9 +36,58 @@ const CharacterProfile = () => {
       justifyContent="center"
       alignItems="center"
     >
-      <Box>
-        <Typography variant="h3">Test Character</Typography>
-        <Typography variant="h5">ID: {characterId}</Typography>
+      <Box textAlign="center">
+        {isLoading ? (
+          <>Loading...</>
+        ) : !isApiError ? (
+          <>
+            <Typography variant="h3">{character.name}</Typography>
+            <Card
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                height: "100%",
+                minWidth: "500px",
+                marginTop: "10px",
+              }}
+            >
+              <CardContent>
+                <Typography color="text.secondary">
+                  Birth Year: {character.birthYear}
+                </Typography>
+                <Typography color="text.secondary">
+                  Eye Color: {character.eyeColor}
+                </Typography>
+                <Typography color="text.secondary">
+                  Height: {character.height}
+                </Typography>
+                <Typography color="text.secondary">
+                  Mass: {character.mass}
+                </Typography>
+                <Typography color="text.secondary">
+                  Skin Color: {character.skinColor}
+                </Typography>
+                <Typography color="text.secondary">
+                  Gender: {character.gender}
+                </Typography>
+                <Typography color="text.secondary">
+                  Hair Color: {character.hairColor}
+                </Typography>
+              </CardContent>
+            </Card>
+          </>
+        ) : (
+          <>Sorry this character does not exist, or there was an error.</>
+        )}
+      </Box>
+      <Box display="flex" justifyContent="start" mt="20px">
+        <Button
+          onClick={() => navigate("/characters")}
+          color="secondary"
+          variant="contained"
+        >
+          See all characters
+        </Button>
       </Box>
     </Box>
   );
